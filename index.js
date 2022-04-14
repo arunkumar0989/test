908af143543f988c9f38ca00164e0e7c8ad71ce0
arunkumar0989 <85949429+arunkumar0989@users.noreply.github.com>
2022-04-14 22:25:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const port = 2001;
+const dir = path.join(__dirname, './test.html')
+
+app.use(express.static(dir), (req, res) => {
+    res.sendFile(dir);
+});
+
+app.listen(port);
